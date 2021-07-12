8f10e5d57640eaedd56f88e92f24edca75d89d02
Daniel Farkas <Daniel@aviva-laptop.local>
2021-07-11 20:06:10 -0400

    switched from jquery to js

--- js/switch_colors.js
+++ js/switch_colors.js
@@ -1,21 +1,23 @@
-$(function () {
-   $(".card").on('click', function(e) {
-      switch ($(this).getAttribute('data-present')) {
-         case 'present':
-            left(e.target);
-            break;
-         case 'absent':
-            came(e.target);
-            break;
-      }
-   });
-});
+document.addEventListener('click', function (e) {
+   updateTag(e);
+}, false);
+
+function updateTag(e) {
+   switch (e.target.getAttribute('data-present')) {
+      case 'present':
+         left(e.target);
+         break;
+      case 'absent':
+         came(e.target);
+         break;
+   }
 
 
-function left(target) {
-   target.setAtribute('data-present', 'absent');
-}
+   function left(target) {
+      target.setAtribute('data-present', 'absent');
+   }
 
-function came(target) {
-   target.setAtribute('data-present', 'absent');
+   function came(target) {
+      target.setAtribute('data-present', 'absent');
+   }
 }
